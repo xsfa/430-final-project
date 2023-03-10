7ac1353719a44654e0422f81d1e4dd1a5efbaa24
Egor Kolyshkin <65327703+EKolyshkin@users.noreply.github.com>
2023-03-09 18:20:44 -0800

    initial formatting

--- SuperBlock.java
+++ SuperBlock.java
@@ -1,30 +1,33 @@
-public class Superblock { 
-    public int totalBlocks; // the number of disk blocks 
-    public int totalInodes; // the number of inodes 
-    public int freeList;    // the block number of the free list's head 
- }
+public class SuperBlock {
+    public int totalBlocks; // the number of disk blocks
+    public int totalInodes; // the number of inodes
+    public int freeList; // the block number of the free list's head
 
- public SuperBlock( int diskSize ) { 
-      // read the superblock from disk.   
-      // check disk contents are valid. 
-      // if invalid, call format( ).                                                                                        
-  } 
+    public SuperBlock(int diskSize) {
+        // read the superblock from disk.
+        // check disk contents are valid.
+        // if invalid, call format( ).
+    }
 
-  void sync( ) { 
-      // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock );   
-  } 
+    void sync() {
+        // write back in-memory superblock to disk: SysLib.rawwrite( 0, superblock );
+    }
 
-  void format( int files ) { 
-      // initialize the superblock  
-      // initialize each inode and immediately write it back to disk 
-      // initialize free blocks 
-  } 
-  public int getFreeBlock( ) { 
-      // get a new free block from the freelist                                                                                 
-      return freeBlockNumber; 
-  } 
+    void format(int files) {
+        // initialize the superblock
+        // initialize each inode and immediately write it back to disk
+        // initialize free blocks
+    }
 
-  public boolean returnBlock( int oldBlockNumber ) { 
-      // return this old block to the free list. The list can be a stack.                                                                                                 
-      //   return true or false; 
-  } 
+    public int getFreeBlock() {
+        int freeBlockNumber = 0;
+        // get a new free block from the freelist
+        return freeBlockNumber;
+    }
+
+    public boolean returnBlock(int oldBlockNumber) {
+        // return this old block to the free list. The list can be a stack.
+        // return true or false;
+        return false;
+    }
+}
