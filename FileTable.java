d3cbab647c0bc2d84d67740b0a5788df992b3b8d
Egor Kolyshkin <65327703+EKolyshkin@users.noreply.github.com>
2023-03-11 13:41:08 -0800

    FileTable ffree() passes all tests, added flags

--- FileTable.java
+++ FileTable.java
@@ -1,6 +1,16 @@
+import java.util.Vector;
+
 public class FileTable {
    private Vector table; // the actual entity of this file table
    private Directory dir; // the root directory
+
+   // define inode flags
+   final static char UNUSED = 0; // 0 = unused
+   final static char USED_r = 1; // 1 = used(r)
+   final static char USED_1r = 2; // 2 = used(!r)
+   final static char UNUSED_w = 3; // 3 = unused(wreg)
+   final static char USED_rw = 4; // 4 = used(r,wreg)
+   final static char USED_1rw = 5; // 5 = used(!r,wreg)
 
    public FileTable(Directory directory) { // constructor
       table = new Vector(); // instantiate a file (structure) table
@@ -14,14 +24,32 @@ public class FileTable {
       // increment this inode's count
       // immediately write back this inode to the disk
       // return a reference to this file (structure) table entry
+
+      // Egor: use flags implemented above
    }
 
-   // IMPLEMENT
    public synchronized boolean ffree(FileTableEntry e) {
       // receive a file table entry reference
-      // save the corresponding inode to the disk
-      // free this file table entry.
-      // return true if this file table entry found in my table
+      boolean found = false;
+
+      if (table.removeElement(e)) { // free this file table entry
+         found = true; // return true if entry was found
+         Inode inode = e.inode;
+
+         // reset this inode flag
+         if (inode.flag == USED_r || inode.flag == USED_1r) {
+            inode.flag = UNUSED;
+         } else if (inode.flag == USED_rw || inode.flag == USED_1rw) {
+            inode.flag = UNUSED_w;
+         }
+
+         // save the corresponding inode to the disk
+         inode.count -= 1;
+         inode.toDisk(e.iNumber);
+         notify();
+      }
+
+      return found;
    }
 
    public synchronized boolean fempty() {
